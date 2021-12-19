0b7ceadd2bda4ff5e8c4eccdb71f4d6a3ea4fc98
Christian Gama <christiangama.dev@gmail.com>
2021-12-19 17:42:02 -0300

    chore: configure babel to be default preset of jest and add new fonts to html

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,19 @@
 module.exports = {
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!**/node_modules/**', '!<rootDir>/src/main/**/*'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/**/*.css.{ts,tsx}'
+  ],
   coverageDirectory: 'coverage',
   roots: ['<rootDir>/src'],
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest'
+    '^.+\\.(ts|tsx)$': ['babel-jest', { configFile: './babel-jest.config.js' }]
   },
   moduleNameMapper: {
-    '@/(.*)$': '<rootDir>/src/$1',
-    '^.+\\.css$': 'identity-obj-proxy'
+    '@/(.*)$': '<rootDir>/src/$1'
   },
   resetMocks: true,
-  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$', '^.+\\.css$'],
-  setupFilesAfterEnv: ['<rootDir>/src/main/config/jest/setupTests.ts']
+  setupFilesAfterEnv: ['<rootDir>/src/main/config/jest/jest-dom.ts']
 }
