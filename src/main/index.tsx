41724cb20df69bb839f073a5cda2ccdf0694c90d
Christian Gama <christiangama.dev@gmail.com>
2021-12-19 02:53:50 -0300

    feat: render the first element through react dom

---
+++ src/main/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '@/presentation/styles/global.css'
+
+const element = <div>Olá, mundo!</div>
+
+ReactDOM.render(element, document.getElementById('root'))
