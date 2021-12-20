79faa5ace13b959c87e18aeee445fd40760a6bd1
Christian Gama <christiangama.dev@gmail.com>
2021-12-20 14:11:20 -0300

    test: implement EyeIcon component

---
+++ src/presentation/components/icons/eyeIcon/__tests__/EyeIcon.spec.tsx
@@ -0,0 +1,29 @@
+import { cleanup, render, screen } from '@testing-library/react'
+import React from 'react'
+import EyeIcon from '../EyeIcon'
+
+const makeSut = (showPassword: boolean): void => {
+  render(<EyeIcon showPassword={showPassword} />)
+}
+
+describe('EyeIcon', () => {
+  afterEach(() => {
+    cleanup()
+  })
+
+  it('should display eyeOpenIcon if showPassword is false', () => {
+    makeSut(false)
+
+    const icon = screen.getByTestId('eyeOpenIcon')
+
+    expect(icon).toBeInTheDocument()
+  })
+
+  it('should display eyeClosedIcon if showPassword is true', () => {
+    makeSut(true)
+
+    const icon = screen.getByTestId('eyeClosedIcon')
+
+    expect(icon).toBeInTheDocument()
+  })
+})
