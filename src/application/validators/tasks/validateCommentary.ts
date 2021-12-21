fd0c0cd172fd09ada0187ed7542bfabef2d2a07a
Christian Gama <christiangama.dev@gmail.com>
2021-12-21 01:10:04 -0300

    feat: implement validateCommentary

---
+++ src/application/validators/tasks/validateCommentary.ts
@@ -0,0 +1,11 @@
+import IValidator from '@/domain/validators/validator.model'
+
+const validateCommentary = (value: string): IValidator => {
+  if (value.length > 400) {
+    return { field: 'commentary', isValid: false, error: 'A observação deve ter no máximo 400 caractéres.' }
+  }
+
+  return { field: 'commentary', isValid: true, error: '' }
+}
+
+export default validateCommentary
