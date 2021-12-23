7d7ed09748ae260369af946e7b7378b9daa529df
Christian Gama - LInux <christiangama.dev@gmail.com>
2021-12-23 04:52:23 -0300

    chore: change settings

--- jest-unit.config.js
+++ jest-unit.config.js
@@ -1,6 +1,6 @@
 const config = require('./jest.config')
 config.testMatch = ['<rootDir>/src/**/*.spec.{js,jsx,ts,tsx}']
-config.maxWorkers = 8
+config.maxWorkers = 12
 config.displayName = {
   color: 'magenta',
   name: 'UNIT'
