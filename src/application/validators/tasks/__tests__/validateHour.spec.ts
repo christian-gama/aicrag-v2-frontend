32b023400a1bc3a6fc6c363d6103569964c62163
Christian Gama - LInux <christiangama.dev@gmail.com>
2021-12-26 03:02:36 -0300

    test: ensure hours is valid

---
+++ src/application/validators/tasks/__tests__/validateHour.spec.ts
@@ -0,0 +1,28 @@
+import InvalidInputError from '@/application/errors/invalidInputError'
+import validateHour from '../validateHour'
+
+describe('validateHour', () => {
+  describe('failure', () => {
+    it('should return an InvalidInputError if the value does not match numbers', () => {
+      expect(validateHour('a')).toBeInstanceOf(InvalidInputError)
+    })
+
+    it('should return an InvalidInputError if the minute is greater than 24', () => {
+      expect(validateHour('24')).toBeInstanceOf(InvalidInputError)
+    })
+
+    it('should return an InvalidInputError if the minute is lesser than 0', () => {
+      expect(validateHour('-1')).toBeInstanceOf(InvalidInputError)
+    })
+
+    it('should return an InvalidInputError if the value is missing', () => {
+      expect(validateHour('')).toBeInstanceOf(InvalidInputError)
+    })
+  })
+
+  describe('success', () => {
+    it('should return undefined if succeeds', () => {
+      expect(validateHour('0')).toBeUndefined()
+    })
+  })
+})
