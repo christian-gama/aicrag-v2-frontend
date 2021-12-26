f670fd3010174cc0e5844ac50c72f9b59efdba0c
Christian Gama - LInux <christiangama.dev@gmail.com>
2021-12-26 03:04:42 -0300

    feat: ensure the time is increased or decreased correctly

---
+++ src/utils/timerIncreaser.ts
@@ -0,0 +1,41 @@
+import getFormattedTime from './getFormattedTime'
+
+const timerIncreaser = (config: { type: 'hour' | 'minute', prevTime: string, increase: boolean }): string => {
+  const { type, prevTime, increase } = config
+
+  if (increase) {
+    const nextTime = parseInt(prevTime) + 1
+
+    if (type === 'hour') {
+      if (nextTime >= 24) {
+        return '00'
+      }
+    }
+
+    if (type === 'minute') {
+      if (nextTime >= 60) {
+        return '00'
+      }
+    }
+
+    return getFormattedTime(nextTime)
+  }
+
+  const nextTime = parseInt(prevTime) - 1
+
+  if (type === 'hour') {
+    if (nextTime < 0) {
+      return '23'
+    }
+  }
+
+  if (type === 'minute') {
+    if (nextTime < 0) {
+      return '59'
+    }
+  }
+
+  return getFormattedTime(nextTime)
+}
+
+export default timerIncreaser
