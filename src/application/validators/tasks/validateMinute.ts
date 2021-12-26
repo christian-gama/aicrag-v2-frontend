194e2affc09d01d092ea8825a229a5effcd9a451
Christian Gama - LInux <christiangama.dev@gmail.com>
2021-12-26 03:02:56 -0300

    test: ensure minutes is valid

---
+++ src/application/validators/tasks/validateMinute.ts
@@ -0,0 +1,11 @@
+import InvalidInputError from '@/application/errors/invalidInputError'
+import IValidator from '../protocols/validator.model'
+
+const validateMinute: IValidator = (value) => {
+  if (!value) return new InvalidInputError('hora', 'é obrigatório')
+  if (isNaN(+value)) return new InvalidInputError('minuto', 'deve ser um número')
+  if (+value > 59) return new InvalidInputError('minuto', 'deve ser menor ou igual a 59')
+  if (+value < 0) return new InvalidInputError('minuto', 'deve ser maior ou igual a 0')
+}
+
+export default validateMinute
