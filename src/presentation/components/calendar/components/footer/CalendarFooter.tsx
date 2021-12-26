a9c5db2e1c0d7f6540132617502e5511143b9858
Christian Gama - LInux <christiangama.dev@gmail.com>
2021-12-26 04:11:16 -0300

    feat: add cancelHandler and confirmHandler to buttons

--- src/presentation/components/calendar/components/footer/CalendarFooter.tsx
+++ src/presentation/components/calendar/components/footer/CalendarFooter.tsx
@@ -1,31 +1,34 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { AppDispatch } from '@/infra/store'
+import { closeModal } from '@/infra/store/modal'
 import Button from '../../../UI/button/Button'
 import { calendarFooterClasses } from './CalendarFooter.css'
 import CalendarTimer from './CalendarTimer'
 
 export const CalendarFooter: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>()
+
+  const cancelHandler = (): void => {
+    dispatch(closeModal())
+  }
+
+  const confirmHandler = (): void => {
+    dispatch(closeModal())
+  }
+
   return (
     <div className={calendarFooterClasses.calendarFooterStyle} data-testid="calendar-footer">
       <CalendarTimer />
 
       <div className={calendarFooterClasses.calendarButtonContainer}>
-        <Button
-          style={{
-            mode: 'text',
-            size: 'sm'
-          }}
-        >
+        <Button testid="cancel-button" style={{ mode: 'text', size: 'sm' }} onClick={cancelHandler}>
           Cancelar
         </Button>
 
         <span style={{ marginLeft: '1.2rem' }} />
 
-        <Button
-          style={{
-            mode: 'contained',
-            size: 'sm'
-          }}
-        >
+        <Button testid="confirm-button" style={{ mode: 'contained', size: 'sm' }} onClick={confirmHandler}>
           Salvar
         </Button>
       </div>
