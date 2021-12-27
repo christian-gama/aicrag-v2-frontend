58dc6c2acf40028bb4b02712f379571db5fc703f
Christian Gama - LInux <christiangama.dev@gmail.com>
2021-12-27 00:27:07 -0300

    chore: set jest maxWorkers to 6

--- jest-unit.config.js
+++ jest-unit.config.js
@@ -1,6 +1,6 @@
 const config = require('./jest.config')
 config.testMatch = ['<rootDir>/src/**/*.spec.{js,jsx,ts,tsx}']
-config.maxWorkers = 12
+config.maxWorkers = 6
 config.displayName = {
   color: 'magenta',
   name: 'UNIT'
