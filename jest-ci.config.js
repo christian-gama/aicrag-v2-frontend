029415535cc3b7f0edc561d3950aa0ffe2ac17f4
Christian Gama - LInux <christiangama.dev@gmail.com>
2021-12-29 00:35:53 -0300

    chore: add test:ci script

---
+++ jest-ci.config.js
@@ -0,0 +1,8 @@
+const config = require('./jest.config')
+config.testMatch = ['<rootDir>/src/**/*.spec.{js,jsx,ts,tsx}', '<rootDir>/src/**/*.test.{js,jsx,ts,tsx}']
+config.maxWorkers = 6
+config.displayName = {
+  color: 'cyan',
+  name: 'CI'
+}
+module.exports = config
