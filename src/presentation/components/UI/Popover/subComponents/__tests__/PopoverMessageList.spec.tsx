7cfcd6f6ad5ab46e6d87857fd511ca4eee7763dc
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-02 19:21:59 -0300

    test: implement PopoverMessageList

---
+++ src/presentation/components/UI/Popover/subComponents/__tests__/PopoverMessageList.spec.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import PopoverMessageList from '../PopoverMessageList'
+
+describe('PopoverMessageList', () => {
+  it('should render the message from messages', () => {
+    const messages = ['message 1', 'message 2']
+
+    render(<PopoverMessageList messages={messages} />)
+
+    expect(screen.getByText(messages[0])).toBeInTheDocument()
+    expect(screen.getByText(messages[1])).toBeInTheDocument()
+  })
+})
