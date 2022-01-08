de6577f6eb308912201fa69be0cccbe126552fbd
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-08 20:25:42 -0300

    feat: add breakpoints

---
+++ src/application/common/breakpoints.css.ts
@@ -0,0 +1,6 @@
+export const breakpoints = {
+  mobile: 'screen and (max-width: 520px)',
+  tablet: 'screen and (max-width: 820px)',
+  desktop: 'screen and (max-width: 1024px)',
+  widescreen: 'screen and (max-width: 1366px)'
+}
