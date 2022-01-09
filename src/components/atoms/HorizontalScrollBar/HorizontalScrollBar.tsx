0a0bf569779a29ebc7430d82e508414ebd5a15c4
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-09 19:01:10 -0300

    test: ensure HorizontalScrollBar render itself and children correctly

--- src/components/atoms/HorizontalScrollBar/HorizontalScrollBar.tsx
+++ src/components/atoms/HorizontalScrollBar/HorizontalScrollBar.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import { style } from './stylesheet'
 
 const HorizontalScrollBar: React.FC = (props) => {
-  return <div className={style.HorizontalScrollBar}>{props.children}</div>
+  return (
+    <div className={style.HorizontalScrollBar} data-testid="horizontal-scroll-bar">
+      {props.children}
+    </div>
+  )
 }
 
 export default HorizontalScrollBar
