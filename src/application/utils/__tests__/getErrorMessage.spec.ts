4398fdbb7b550c8233364909d1454e02d8437cf4
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-09 00:28:01 -0300

    test: add more generics errors messages

---
+++ src/application/utils/__tests__/getErrorMessage.spec.ts
@@ -0,0 +1,55 @@
+import getErrorMessage from '../getErrorMessage'
+
+describe('getErrorMessage', () => {
+  it('should return the graphql error message', () => {
+    const error = {
+      networkError: {
+        result: {
+          errors: [
+            {
+              message: 'GraphQL error message'
+            }
+          ]
+        }
+      }
+    }
+
+    expect(getErrorMessage(error)).toBe('GraphQL error message')
+  })
+
+  it('should return the network error message', () => {
+    const error = {
+      networkError: {
+        message: 'Network error message'
+      }
+    }
+
+    expect(getErrorMessage(error)).toBe('Houve um problema com a conexão, tente novamente mais tarde')
+  })
+
+  it('should return the default error message', () => {
+    const error = {
+      message: 'Error message'
+    }
+
+    expect(getErrorMessage(error)).toBe('Error message')
+  })
+
+  it('should return the generic error if extension code is BAD_USER_INPUT', () => {
+    const error = {
+      networkError: {
+        result: {
+          errors: [
+            {
+              extensions: {
+                code: 'BAD_USER_INPUT'
+              }
+            }
+          ]
+        }
+      }
+    }
+
+    expect(getErrorMessage(error)).toBe('Algo deu errado, tente novamente mais tarde')
+  })
+})
