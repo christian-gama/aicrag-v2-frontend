6aaeb90057c60e57f6b511c51d1998184fc6937c
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-09 05:10:32 -0300

    feat: add new error utils

--- src/application/utils/getErrorMessage.ts
+++ src/application/utils/getErrorMessage.ts
@@ -1,9 +1,13 @@
+import translateError from './translateError'
+
 const getErrorMessage = (error: any) => {
   const graphQLErrorCode = error.networkError?.result?.errors[0].extensions?.code
   if (graphQLErrorCode === 'BAD_USER_INPUT') return 'Algo deu errado, tente novamente mais tarde'
 
   const graphQLErrorMessage = error.networkError?.result?.errors[0].message
-  if (graphQLErrorMessage) return graphQLErrorMessage
+  if (graphQLErrorMessage) {
+    return translateError(graphQLErrorMessage)
+  }
 
   const networkErrorMessage = error.networkError?.message
   if (networkErrorMessage) return 'Houve um problema com a conexão, tente novamente mais tarde'
---
+++ src/application/utils/translateInputName.ts
@@ -0,0 +1,20 @@
+const translateInputName = (inputName: string): string => {
+  switch (inputName.toLowerCase()) {
+    case 'name':
+      return 'Nome'
+
+    case 'email':
+      return 'Email'
+
+    case 'password':
+      return 'Senha'
+
+    case 'passwordconfirmation':
+      return 'Confirmação de senha'
+
+    default:
+      return inputName
+  }
+}
+
+export default translateInputName
