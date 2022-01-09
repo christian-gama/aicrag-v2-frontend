4c631db046468ad86f0d67bf61649269fe4987e7
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-09 15:57:38 -0300

    refactor: remove function from test

--- src/helpers/__tests__/translateError.spec.ts
+++ src/helpers/__tests__/translateError.spec.ts
@@ -1,26 +1,4 @@
-import translateInputName from '../translateInputName'
-
-const translateError = (error: any) => {
-  if (typeof error !== 'string') return error
-
-  let field = error.split(':')
-
-  if (field.length > 1) {
-    const newError = error.replace(field[1].trim(), translateInputName(field[1].trim()))
-
-    if (newError === error) {
-      field = error.split(' ')
-
-      if (field.length > 1) {
-        return error.replace(field[0].trim(), translateInputName(field[0].trim()))
-      }
-    }
-
-    return newError
-  }
-
-  return error
-}
+import translateError from '../translateError'
 
 describe('translateError', () => {
   it('should return the same error when it is not a string', () => {
