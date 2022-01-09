aafcdfc70ab329c33c60b333c6aad3836af0d63e
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-09 00:27:40 -0300

    feat: add more generics errors messages

--- src/application/utils/getErrorMessage.ts
+++ src/application/utils/getErrorMessage.ts
@@ -1,7 +1,12 @@
 const getErrorMessage = (error: any) => {
-  const graphQLErrorMessage = error.networkError?.result?.errors[0].message
+  const graphQLErrorCode = error.networkError?.result?.errors[0].extensions?.code
+  if (graphQLErrorCode === 'BAD_USER_INPUT') return 'Algo deu errado, tente novamente mais tarde'
 
+  const graphQLErrorMessage = error.networkError?.result?.errors[0].message
   if (graphQLErrorMessage) return graphQLErrorMessage
+
+  const networkErrorMessage = error.networkError?.message
+  if (networkErrorMessage) return 'Houve um problema com a conexão, tente novamente mais tarde'
 
   return error.message
 }
