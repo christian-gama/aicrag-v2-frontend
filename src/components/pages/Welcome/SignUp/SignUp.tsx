8739b5d285008b8f46d7cca16b456b16e7611fce
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-09 07:28:26 -0300

    refactor: move pages from presentation to components

--- src/components/pages/Welcome/SignUp/SignUp.tsx
+++ src/components/pages/Welcome/SignUp/SignUp.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import SignUpForm from '@/components/templates/forms/SignUpForm'
 import FormProvider from '@/application/models/context/form/FormProvider'
-import WelcomeCard from '../../../../components/templates/WelcomeCard/WelcomeCard'
+import WelcomeCard from '../../../templates/WelcomeCard/WelcomeCard'
 
 const SignUp: React.FC = () => {
   return (
--- src/external/router/WelcomeRoute.tsx
+++ src/external/router/WelcomeRoute.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-import SignIn from '@/presentation/pages/Welcome/SignIn'
-import SignUp from '@/presentation/pages/Welcome/SignUp'
+import SignIn from '@/components/pages/Welcome/SignIn'
+import SignUp from '@/components/pages/Welcome/SignUp'
 
 export default (
   <Route path="/boas-vindas">
