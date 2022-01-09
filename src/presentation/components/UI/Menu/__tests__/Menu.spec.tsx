3bb89b0f52f3e79b53bfce0c7bf53903a906c3f3
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-09 01:38:31 -0300

    test: implement Menu component that contain navigation

---
+++ src/presentation/components/UI/Menu/__tests__/Menu.spec.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Menu from '../Menu'
+import MenuProps from '../Menu.model'
+
+const makeSut = (props: MenuProps) => {
+  return render(
+    <>
+      <BrowserRouter>
+        <Routes location={'/'}>
+          <Route path={'/'} element={<Menu {...props} />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+describe('Menu', () => {
+  it('should have the correct href', () => {
+    const buttons = ['Home', 'About', 'Contact']
+    const url = '/teste/url/'
+    makeSut({ buttons, url })
+
+    const link = screen.getByTestId('menu-link-home')
+
+    expect(link).toHaveAttribute('href', '/teste/url/home')
+  })
+})
