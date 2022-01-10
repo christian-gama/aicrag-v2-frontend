fed1b8fbd11966baa78b997c812f801ae39fe180
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-09 23:58:52 -0300

    test: ensure Calendar opens when calling openCalendar action

--- src/components/organisms/Calendar/__tests__/Calendar.spec.tsx
+++ src/components/organisms/Calendar/__tests__/Calendar.spec.tsx
@@ -1,11 +1,43 @@
 import render from '@/../tests/config/renderWithProvider'
 import calendarStoreMock from '@/../tests/mocks/calendarStore.mock'
-import { screen } from '@testing-library/react'
+import { calendarActions } from '@/context/models/calendar/calendar.actions'
+import { AppDispatch } from '@/context/store'
+import { screen, fireEvent } from '@testing-library/react'
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import Calendar from '..'
 
+const DummyButton = () => {
+  const dispatch = useDispatch<AppDispatch>()
+
+  const openCalendarHandler = () => {
+    dispatch(calendarActions.openCalendar())
+  }
+
+  const closeCalendarHandler = () => {
+    dispatch(calendarActions.closeCalendar())
+  }
+
+  return (
+    <div>
+      <button onClick={openCalendarHandler} data-testid="open-button">
+        OpenCalendar
+      </button>
+      <button onClick={closeCalendarHandler} data-testid="close-button">
+        CloseCalendar
+      </button>
+    </div>
+  )
+}
+
 const makeSut = (): void => {
-  render(<Calendar previousDate={Date.now()} />, { ...calendarStoreMock })
+  render(
+    <div>
+      <DummyButton />
+      <Calendar previousDate={Date.now()} />
+    </div>,
+    { ...calendarStoreMock }
+  )
 }
 
 describe('Calendar', () => {
@@ -30,5 +62,19 @@ describe('Calendar', () => {
     backdrop.dispatchEvent(new MouseEvent('click', { bubbles: true }))
 
     expect(backdrop).not.toBeInTheDocument()
+  })
+
+  it('should open the calendar using the openCalendar action if the calendar is not open', () => {
+    makeSut()
+
+    let calendarContainer = screen.getByTestId('calendar-wrapper')
+    const closeCalendarButton = screen.getByTestId('close-button')
+    fireEvent.click(closeCalendarButton)
+    expect(calendarContainer).not.toBeInTheDocument()
+
+    const openCalendarButton = screen.getByTestId('open-button')
+    fireEvent.click(openCalendarButton)
+    calendarContainer = screen.getByTestId('calendar-wrapper')
+    expect(calendarContainer).toBeInTheDocument()
   })
 })
