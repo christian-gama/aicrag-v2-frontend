cca36ef5efbdf94788e68c40c4cf87f175794100
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-10 00:42:25 -0300

    test: add more assertions

--- src/helpers/__tests__/translateError.spec.ts
+++ src/helpers/__tests__/translateError.spec.ts
@@ -14,5 +14,9 @@ describe('translateError', () => {
     expect(translateError('field: reason')).toBe('field: reason')
     expect(translateError('Name inválido: reason')).toBe('Nome inválido: reason')
     expect(translateError('Parâmetro inválido: name')).toBe('Parâmetro inválido: Nome')
+    expect(translateError('Parâmetro inválido: name, email')).toBe('Parâmetro inválido: Nome, Email')
+    expect(translateError('O campo "passwordConfirmation" é inválido.')).toBe(
+      'O campo "Confirmação de senha" é inválido.'
+    )
   })
 })
