1ef0e0ce29569aca9ee483fcd7c5a31ae0001f85
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-10 00:36:17 -0300

    refactor: make the function cleaner and simplier

--- src/helpers/translateError.ts
+++ src/helpers/translateError.ts
@@ -3,23 +3,10 @@ import translateInputName from './translateInputName'
 const translateError = (error: any) => {
   if (typeof error !== 'string') return error
 
-  let field = error.split(':')
-
-  if (field.length > 1) {
-    const newError = error.replace(field[1].trim(), translateInputName(field[1].trim()))
-
-    if (newError === error) {
-      field = error.split(' ')
-
-      if (field.length > 1) {
-        return error.replace(field[0].trim(), translateInputName(field[0].trim()))
-      }
-    }
-
-    return newError
-  }
-
   return error
+    .split(/( |:|,|\.|@|;|!|"|')/)
+    .map(translateInputName)
+    .join('')
 }
 
 export default translateError
