5e70bd8abd4ea16ade7480defd39aa2388089c5f
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-12 23:01:27 -0300

    test: ensure error return the correct message

---
+++ src/services/errors/__tests__/internalError.spec.ts
@@ -0,0 +1,15 @@
+import InternalError from '../internalError'
+
+describe('InvalidInputError', () => {
+  it('should be a class', () => {
+    expect(typeof InternalError).toBe('function')
+  })
+
+  it('should extend Error', () => {
+    expect(InternalError.prototype instanceof Error).toBe(true)
+  })
+
+  it('should have a message', () => {
+    expect(new InternalError().message).toBe('Algo deu errado, tente novamente mais tarde')
+  })
+})
---
+++ src/services/errors/__tests__/networkError.spec.ts
@@ -0,0 +1,15 @@
+import NetworkError from '../connectionError'
+
+describe('InvalidInputError', () => {
+  it('should be a class', () => {
+    expect(typeof NetworkError).toBe('function')
+  })
+
+  it('should extend Error', () => {
+    expect(NetworkError.prototype instanceof Error).toBe(true)
+  })
+
+  it('should have a message', () => {
+    expect(new NetworkError().message).toBe('Houve um erro inesperado de conexão')
+  })
+})
