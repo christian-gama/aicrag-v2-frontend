d057e6ecea2618e1eaab35bbe27911a71a7495e3
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-13 00:00:38 -0300

    refactor: pass API_URL as value for uri

--- src/external/graphql/config/apolloClient.ts
+++ src/external/graphql/config/apolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink, from } from '@apollo/client'
 import authLink from '../links/authLink'
 import authErrorLink from '../links/errorLink'
 
-const link = from([authErrorLink, authLink, new HttpLink({ uri: 'http://localhost:4000/graphql' })])
+const link = from([authErrorLink, authLink, new HttpLink({ uri: process.env.API_URL })])
 
 const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
