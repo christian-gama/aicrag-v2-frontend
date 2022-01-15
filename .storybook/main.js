4a55c8da7ca8be4e25ee8e68f2837d40c5ec9c26
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-14 21:44:47 -0300

    chore: set up Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = {
+  stories: ['../src/components/**/*.stories.tsx'],
+  addons: [
+    path.resolve(__dirname, 'vanillaExtract'),
+    path.resolve(__dirname, 'pathAlias'),
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@storybook/addon-storysource'
+  ],
+  core: {
+    builder: 'storybook-builder-vite'
+  }
+}
---
+++ .storybook/pathAlias.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+  async viteFinal(config) {
+    return {
+      ...config,
+      resolve: {
+        alias: {
+          '@/components': path.resolve(__dirname, '../src/components')
+        }
+      }
+    }
+  }
+}
