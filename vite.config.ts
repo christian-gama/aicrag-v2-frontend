b319504d3aebef2ff28c8c8a568260d41157d512
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-17 12:01:00 -0300

    chore: add plugin to remove HTML attributes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, Plugin } from 'vite'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import path from 'path'
+
+const removeAttributes = (options: { attributes: string[] }): Plugin => {
+  return {
+    apply: 'build',
+    name: 'remove-attributes',
+    transform(code) {
+      options.attributes.forEach((attribute) => {
+        const attributeMatcher = new RegExp(
+          `(,\\s*\\"${attribute}\\" *: *\\".*\\"|(?=\\s*\\}))|(\\s*\\"${attribute}\\" *: *\\".*\\"(,|(?=\\s*\\})))`,
+          'g'
+        )
+
+        code = code.replace(attributeMatcher, '')
+      })
+
+      return {
+        code
+      }
+    }
+  }
+}
 
 export default defineConfig({
   resolve: {
@@ -16,5 +37,5 @@ export default defineConfig({
 
   publicDir: 'public',
 
-  plugins: [reactRefresh(), vanillaExtractPlugin()]
+  plugins: [reactRefresh(), vanillaExtractPlugin(), removeAttributes({ attributes: ['data-testid'] })]
 })
