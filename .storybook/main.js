53f3420a5e2c68bb52122c6d12a1f21f69dee437
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-17 12:00:37 -0300

    chore: remove unused dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,7 +8,6 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-a11y',
-    '@storybook/addon-storysource',
     '@storybook/addon-actions'
   ],
   core: {
