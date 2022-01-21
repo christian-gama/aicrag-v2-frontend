91948aaaa672626a2f44570a99fbac857201e532
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-21 19:25:50 -0300

    refactor: remove unused dependencies (yagni)

--- tests/e2e/cypress/plugins/index.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/triple-slash-reference */
-/// <reference types="cypress" />
-/**
- * @type {Cypress.PluginConfig}
- */
-module.exports = (on: any, config: any) => {}
