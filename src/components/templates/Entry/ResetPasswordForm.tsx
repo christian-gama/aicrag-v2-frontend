49855ddabb8c4334fd621e9f58006ff14e6c228e
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-28 14:56:50 -0300

    feat: submits the form

--- src/components/templates/Entry/ResetPasswordForm.tsx
+++ src/components/templates/Entry/ResetPasswordForm.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import ControlForm from '@/components/organisms/Control/ControlForm'
+import { useVerifyResetPasswordTokenQuery } from '@/external/graphql/generated'
 
 const ResetPasswordForm: React.FC = () => {
-  const hasValidParam = true
+  const params = useParams()
 
-  return (
-    hasValidParam && (
-      <div data-testid="reset-password">
-        <ControlForm submitHandler={async () => {}}></ControlForm>
-      </div>
-    )
-  )
+  const { token } = params
+  const { data } = useVerifyResetPasswordTokenQuery({
+    variables: {
+      token
+    }
+  })
+
+  return data
+    ? (
+    <div data-testid="reset-password">
+      <ControlForm submitHandler={async () => {}}>
+        {`${token!} token`}
+      </ControlForm>
+    </div>
+      )
+    : null
 }
 
 export default ResetPasswordForm
