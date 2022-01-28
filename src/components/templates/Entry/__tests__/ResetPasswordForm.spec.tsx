80f09b73bade4fcc61946bc861f7c8c9c386bab2
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-28 14:57:05 -0300

    test: submits the form

--- src/components/templates/Entry/__tests__/ResetPasswordForm.spec.tsx
+++ src/components/templates/Entry/__tests__/ResetPasswordForm.spec.tsx
@@ -1,22 +1,52 @@
-import { screen } from '@testing-library/react'
+import { fireEvent } from '@storybook/testing-library'
+import { cleanup, screen, waitFor } from '@testing-library/react'
 import React from 'react'
+import OverlayRoot from '@/tests/helpers/overlayRoot'
 import renderWithProviders from '@/tests/helpers/renderWithProviders'
+import verifyResetPasswordTokenMock from '@/tests/mocks/queries/verifyResetPasswordToken.mock'
 import ResetPasswordForm from '../ResetPasswordForm'
 
 describe('ResetPasswordForm', () => {
-  it('renders correctly', () => {
-    renderWithProviders(<ResetPasswordForm />)
+  const overlayRoot = new OverlayRoot()
 
+  afterEach(() => {
+    cleanup()
+    overlayRoot.removeOverlayRoot()
+  })
+
+  beforeEach(() => {
+    overlayRoot.addOverlayRoot()
+  })
+
+  it('renders correctly', async () => {
+    renderWithProviders(<ResetPasswordForm />, {
+      apolloMocks: [verifyResetPasswordTokenMock()]
+    })
+    await waitFor(() => screen.getByTestId('form'))
     const resetPassword = screen.getByTestId('reset-password')
 
     expect(resetPassword).toBeInTheDocument()
   })
 
-  it('displays the form if params is valid', () => {
-    renderWithProviders(<ResetPasswordForm />)
-
+  it('displays the form if params is valid', async () => {
+    renderWithProviders(<ResetPasswordForm />, {
+      apolloMocks: [verifyResetPasswordTokenMock()]
+    })
+    await waitFor(() => screen.getByTestId('form'))
     const form = screen.getByTestId('form')
 
     expect(form).toBeInTheDocument()
+  })
+
+  it('submits the form', async () => {
+    renderWithProviders(<ResetPasswordForm />, {
+      apolloMocks: [verifyResetPasswordTokenMock()]
+    })
+    await waitFor(() => screen.getByTestId('form'))
+    const form = screen.getByTestId('form')
+
+    fireEvent.submit(form)
+
+    await waitFor(() => expect(form).toBeInTheDocument())
   })
 })
