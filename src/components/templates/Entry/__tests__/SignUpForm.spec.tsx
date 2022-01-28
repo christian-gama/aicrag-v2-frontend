22c71e2b6ade6a1549f32f7ff406ca70f361c2fe
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-28 01:12:11 -0300

    refactor: rewrite tests to be simpler and easier to read

--- src/components/templates/Entry/__tests__/SignUpForm.spec.tsx
+++ src/components/templates/Entry/__tests__/SignUpForm.spec.tsx
@@ -1,36 +1,15 @@
-import { MockedProvider, MockedResponse } from '@apollo/client/testing'
-import {
-  cleanup,
-  fireEvent,
-  render,
-  screen,
-  waitFor
-} from '@testing-library/react'
+import { cleanup, fireEvent, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import React from 'react'
 import { act } from 'react-dom/test-utils'
-import { MemoryRouter, Routes, Route } from 'react-router-dom'
-import FormProvider from '@/context/models/form/form.provider'
+import { popoverVar } from '@/external/graphql/reactiveVars/popoverVar'
 import OverlayRoot from '@/tests/helpers/overlayRoot'
+import renderWithProviders from '@/tests/helpers/renderWithProviders'
 import sleep from '@/tests/helpers/sleep'
 import sendWelcomeEmailMock from '@/tests/mocks/queries/sendWelcomeEmail.mock'
 import signUpMock from '@/tests/mocks/queries/signUp.mock'
 import variablesMock from '@/tests/mocks/variables.mock'
 import SignUpForm from '../SignUpForm'
-
-const makeSut = (mocks: Array<MockedResponse<Record<string, any>>>) => {
-  return render(
-    <MockedProvider mocks={mocks} addTypename={false}>
-      <FormProvider>
-        <MemoryRouter initialEntries={['/entry/sign-in']}>
-          <Routes>
-            <Route path="/entry/sign-in" element={<SignUpForm />} />
-          </Routes>
-        </MemoryRouter>
-      </FormProvider>
-    </MockedProvider>
-  )
-}
 
 describe('SignUpForm', () => {
   const overlayRoot = new OverlayRoot()
@@ -44,32 +23,37 @@ describe('SignUpForm', () => {
     overlayRoot.addOverlayRoot()
   })
 
-  it('should render SignUpForm correctly', () => {
-    makeSut([signUpMock()])
-
+  it('renders correctly', () => {
+    renderWithProviders(<SignUpForm />)
     const signInForm = screen.getByTestId('form')
 
     expect(signInForm).toBeInTheDocument()
   })
 
-  it('should submit the form', async () => {
-    makeSut([signUpMock(), sendWelcomeEmailMock()])
-
+  it('submits the form', async () => {
+    const popoverVarSpy = jest.spyOn(popoverVar, 'setPopover')
+    renderWithProviders(<SignUpForm />, {
+      apolloMocks: [signUpMock(), sendWelcomeEmailMock()]
+    })
     const form = screen.getByTestId('form')
-    const inputs = screen.getAllByTestId('base-input')
-    userEvent.type(inputs[0], variablesMock.name)
-    userEvent.type(inputs[1], variablesMock.email)
-    userEvent.type(inputs[2], variablesMock.password)
-    userEvent.type(inputs[3], variablesMock.password)
+    const [name, email, password, passwordConfirmation] =
+      screen.getAllByTestId('base-input')
+    userEvent.type(name, variablesMock.name)
+    userEvent.type(email, variablesMock.email)
+    userEvent.type(password, variablesMock.password)
+    userEvent.type(passwordConfirmation, variablesMock.password)
 
     fireEvent.submit(form)
 
     await act(async () => {
-      await sleep(0)
+      await sleep()
     })
 
     await waitFor(() =>
-      expect(screen.getByTestId('loading-spinner-icon')).toBeInTheDocument()
+      expect(popoverVarSpy).toHaveBeenCalledWith(
+        expect.stringMatching(/\w/gi),
+        'success'
+      )
     )
   })
 })
