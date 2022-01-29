c371cb05b8d206ff93e37f73798aede52078607a
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-28 23:27:11 -0300

    test: display a loading spinner on data loading or redirects on error

--- src/components/templates/Entry/__tests__/ResetPasswordForm.spec.tsx
+++ src/components/templates/Entry/__tests__/ResetPasswordForm.spec.tsx
@@ -1,10 +1,18 @@
 import { fireEvent } from '@storybook/testing-library'
 import { cleanup, screen, waitFor } from '@testing-library/react'
 import React from 'react'
+import { act } from 'react-dom/test-utils'
 import OverlayRoot from '@/tests/helpers/overlayRoot'
 import renderWithProviders from '@/tests/helpers/renderWithProviders'
+import sleep from '@/tests/helpers/sleep'
 import verifyResetPasswordTokenMock from '@/tests/mocks/queries/verifyResetPasswordToken.mock'
 import ResetPasswordForm from '../ResetPasswordForm'
+
+const mockNavigate = jest.fn()
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockNavigate
+}))
 
 describe('ResetPasswordForm', () => {
   const overlayRoot = new OverlayRoot()
@@ -48,5 +56,26 @@ describe('ResetPasswordForm', () => {
     fireEvent.submit(form)
 
     await waitFor(() => expect(form).toBeInTheDocument())
+  })
+
+  it('display loading screen while fetching data', async () => {
+    renderWithProviders(<ResetPasswordForm />, {
+      apolloMocks: [verifyResetPasswordTokenMock()]
+    })
+    const loadingSpinner = screen.getByTestId('loading-spinner-icon')
+
+    expect(loadingSpinner).toBeInTheDocument()
+  })
+
+  it('calls navigate on error', async () => {
+    renderWithProviders(<ResetPasswordForm />, {
+      apolloMocks: [verifyResetPasswordTokenMock(new Error())]
+    })
+
+    await act(async () => {
+      await sleep()
+    })
+
+    expect(mockNavigate).toHaveBeenCalled()
   })
 })
