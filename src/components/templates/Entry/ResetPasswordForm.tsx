cf3456569c14b68337fcbc3352517abd5d7554d5
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-28 23:26:57 -0300

    feat: display a loading spinner on data loading or redirects on error

--- src/components/templates/Entry/ResetPasswordForm.tsx
+++ src/components/templates/Entry/ResetPasswordForm.tsx
@@ -1,27 +1,36 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import ControlForm from '@/components/organisms/Control/ControlForm'
+import Center from '@/components/utils/Center'
+import LoadingSpinnerIcon from '@/components/utils/icons/LoadingSpinnerIcon'
 import { useVerifyResetPasswordTokenQuery } from '@/external/graphql/generated'
 
 const ResetPasswordForm: React.FC = () => {
   const params = useParams()
+  const navigate = useNavigate()
 
   const { token } = params
-  const { data } = useVerifyResetPasswordTokenQuery({
+  const { error, loading } = useVerifyResetPasswordTokenQuery({
     variables: {
       token
     }
   })
 
-  return data
-    ? (
+  if (error) navigate('/404')
+
+  if (loading) {
+    return (
+      <Center>
+        <LoadingSpinnerIcon style={{ size: 'lg' }} />
+      </Center>
+    )
+  }
+
+  return (
     <div data-testid="reset-password">
-      <ControlForm submitHandler={async () => {}}>
-        {`${token!} token`}
-      </ControlForm>
+      <ControlForm submitHandler={async () => {}}></ControlForm>
     </div>
-      )
-    : null
+  )
 }
 
 export default ResetPasswordForm
