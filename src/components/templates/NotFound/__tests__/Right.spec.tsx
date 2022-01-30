7272d33ab0d566bb4cb99c5f77496ad93e679131
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-30 00:05:01 -0300

    test: calls navigate when clicking on Home button

--- src/components/templates/NotFound/__tests__/Right.spec.tsx
+++ src/components/templates/NotFound/__tests__/Right.spec.tsx
@@ -1,10 +1,26 @@
 import { cleanup, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { useWindowDimensions } from '@/components/_hooks'
 import { renderWithProviders } from '@/tests/helpers'
 import { Right } from '../Right'
+
+const mockNavigate = jest.fn()
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockNavigate
+}))
+
+jest.mock('../../../_hooks/useWindowDimensions')
+const useWindowDimensionsMock = useWindowDimensions as jest.Mock
 
 describe('Right', () => {
   afterEach(() => {
     cleanup()
+    useWindowDimensionsMock.mockRestore()
+  })
+
+  beforeEach(() => {
+    useWindowDimensionsMock.mockReturnValue({ width: 521, height: 520 })
   })
 
   it('renders correctly', () => {
@@ -12,5 +28,24 @@ describe('Right', () => {
     const text = screen.getByText(/esta página não está disponível/gi)
 
     expect(text).toBeInTheDocument()
+  })
+
+  it('renders with text even with view width lesser or equal to 520', async () => {
+    useWindowDimensionsMock.mockReturnValue({ width: 520 })
+    renderWithProviders(<Right />)
+    const text = screen.getByText(
+      /o link que você acessou pode estar quebrado/gi
+    )
+
+    expect(text).toBeInTheDocument()
+  })
+
+  it('calls navigate when clicking on Home button', () => {
+    renderWithProviders(<Right />)
+    const button = screen.getByText(/home/i)
+
+    userEvent.click(button)
+
+    expect(mockNavigate).toHaveBeenCalled()
   })
 })
