8b62247dbc8ebe0c6111922e03fda6a57483d786
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-30 00:36:00 -0300

    test: redirects to the correct page when clicking on Link

--- src/components/organisms/Pin/__tests__/Pin.spec.tsx
+++ src/components/organisms/Pin/__tests__/Pin.spec.tsx
@@ -1,5 +1,16 @@
 import { render, cleanup, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { Pin } from '..'
+
+const mockLink = jest.fn()
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  Link: (props: any) => {
+    mockLink(props)
+
+    return <>BackIcon</>
+  }
+}))
 
 describe('Pin', () => {
   afterEach(() => {
@@ -11,5 +22,18 @@ describe('Pin', () => {
     const pin = screen.getByTestId('pin')
 
     expect(pin).toBeInTheDocument()
+  })
+
+  it('redirects to the correct page when clicking on Link', () => {
+    render(<Pin />)
+    const link = screen.getByText(/backicon/i)
+
+    userEvent.click(link)
+
+    expect(mockLink).toHaveBeenCalledWith({
+      children: expect.anything(),
+      'aria-label': expect.anything(),
+      to: '/'
+    })
   })
 })
