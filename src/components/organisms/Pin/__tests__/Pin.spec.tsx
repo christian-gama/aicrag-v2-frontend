3e90fdfac39e42da3476da95d0842a80c2dc00ae
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-31 18:35:29 -0300

    test: calls Steps with responsive props

--- src/components/organisms/Pin/__tests__/Pin.spec.tsx
+++ src/components/organisms/Pin/__tests__/Pin.spec.tsx
@@ -1,6 +1,7 @@
 import { render, cleanup, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { Pin } from '..'
+import { useWindowDimensions } from '@/components/_hooks'
 
 const mockLink = jest.fn()
 jest.mock('react-router-dom', () => ({
@@ -12,9 +13,26 @@ jest.mock('react-router-dom', () => ({
   }
 }))
 
+const mockFunction = jest.fn()
+jest.mock('../../../atoms/Steps', () => ({
+  Steps: (props: any) => {
+    mockFunction(props)
+
+    return <>Steps</>
+  }
+}))
+
+jest.mock('../../../_hooks/useWindowDimensions')
+const mockUseWindowDimensions = useWindowDimensions as jest.Mock
+
 describe('Pin', () => {
   afterEach(() => {
     cleanup()
+    mockUseWindowDimensions.mockReset()
+  })
+
+  beforeEach(() => {
+    mockUseWindowDimensions.mockReturnValue({ width: 1920, height: 1080 })
   })
 
   it('renders correctly', () => {
@@ -37,24 +55,30 @@ describe('Pin', () => {
     })
   })
 
-  it("has an onClick instead of To if it's not a page", () => {
-    render(<Pin isPage={false} isOpen />)
-    const link = screen.getByText(/backicon/i)
-
-    userEvent.click(link)
-
-    expect(mockLink).toHaveBeenCalledWith({
-      children: expect.anything(),
-      'aria-label': expect.anything(),
-      onClick: expect.anything(),
-      to: ''
-    })
-  })
-
   it('does not render if isOpen is false', () => {
     render(<Pin isPage={false} isOpen={false} />)
     const pin = screen.queryByTestId('pin')
 
     expect(pin).not.toBeInTheDocument()
+  })
+
+  it('closes when clicks on Link if it is not a page', () => {
+    render(<Pin isPage={false} isOpen />)
+    const back = screen.getByTestId('pin-back')
+
+    userEvent.click(back)
+
+    expect(screen.queryByTestId('pin')).not.toBeInTheDocument()
+  })
+
+  it('calls Steps with responsive props', () => {
+    mockUseWindowDimensions.mockReturnValue({ width: 520, height: 599 })
+    render(<Pin isPage={false} isOpen />)
+
+    expect(mockFunction).toHaveBeenCalledWith({
+      direction: 'row',
+      gap: '14rem',
+      steps: expect.anything()
+    })
   })
 })
