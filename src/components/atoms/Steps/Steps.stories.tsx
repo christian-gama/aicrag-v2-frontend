4830f9eb5a9fd8b33aa12f8e1db0fcf765ff4ecb
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-31 14:07:29 -0300

    feat: ensure Steps is rendered correctly

---
+++ src/components/atoms/Steps/Steps.stories.tsx
@@ -0,0 +1,9 @@
+import { ComponentStoryObj, ComponentMeta } from '@storybook/react'
+import { Steps } from './Steps'
+
+export default {
+  component: Steps,
+  title: 'atoms/Steps'
+} as ComponentMeta<typeof Steps>
+
+export const Default: ComponentStoryObj<typeof Steps> = {}
---
+++ src/components/atoms/Steps/Steps.tsx
@@ -0,0 +1,5 @@
+import * as classes from './stylesheet'
+
+export const Steps: React.FC = () => {
+  return <div className={classes.steps} data-testid="steps"></div>
+}
