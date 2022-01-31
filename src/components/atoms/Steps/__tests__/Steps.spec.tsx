3b516cc560ed61bbfcbdb2c868c0b67357f72669
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-01-31 15:50:32 -0300

    test: ensure renders CheckIcon based on steps check

--- src/components/atoms/Steps/__tests__/Steps.spec.tsx
+++ src/components/atoms/Steps/__tests__/Steps.spec.tsx
@@ -7,9 +7,23 @@ describe('Steps', () => {
   })
 
   it('renders correctly', () => {
-    render(<Steps />)
+    render(<Steps steps={[]} />)
     const steps = screen.getByTestId('steps')
 
     expect(steps).toBeInTheDocument()
+  })
+
+  it('renders a CheckIcon if stepItem is checked', () => {
+    render(<Steps steps={[{ label: 'Any label', check: true }]} />)
+    const checkIcon = screen.getByTestId('check-icon')
+
+    expect(checkIcon).toBeInTheDocument()
+  })
+
+  it('does not render a CheckIcon if stepItem is not checked', () => {
+    render(<Steps steps={[{ label: 'Any label', check: false }]} />)
+    const checkIcon = screen.queryByTestId('check-icon')
+
+    expect(checkIcon).not.toBeInTheDocument()
   })
 })
