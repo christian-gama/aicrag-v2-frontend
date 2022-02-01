ce3c44cbded9ba5480c8030fb46003f0ffff510a
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-02-01 04:14:44 -0300

    feat: add userId and change token to a realistic token value

--- tests/mocks/variables.mock.ts
+++ tests/mocks/variables.mock.ts
@@ -1,8 +1,10 @@
 export const mockVariables = {
+  userId: '597b095a-58e2-4e5a-a015-9f1fa4129670',
   name: 'Any Name',
   email: 'any@email.com',
   password: '123123',
   passwordConfirmation: '123123',
   message: 'Any message',
-  token: 'any_token'
+  token:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1OTdiMDk1YS01OGUyLTRlNWEtYTAxNS05ZjFmYTQxMjk2NzAiLCJpYXQiOjE2NDM2OTkyNjIsImV4cCI6MTY0MzY5OTg2Mn0.y1Ejjq7agytiG82UyDHHvr4tx17DKkmtrfa6eqUvwnE'
 } as const
