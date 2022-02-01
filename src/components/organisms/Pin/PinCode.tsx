feedc35c62058e3005f12ed0fd4c00f51d22f41a
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-02-01 12:15:10 -0300

    refactor: improve code readability by creating a custom hooks

--- src/components/organisms/Pin/PinCode.tsx
+++ src/components/organisms/Pin/PinCode.tsx
@@ -1,174 +1,44 @@
-import { ApolloError } from '@apollo/client'
-import {
-  Dispatch,
-  FocusEvent,
-  FormEvent,
-  MouseEvent,
-  SetStateAction,
-  useEffect,
-  useState
-} from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getUserByToken } from '@/services/token/getUserByToken'
-import {
-  mailerCountdownActions,
-  MailerCountdownStates
-} from '@/context/models/mailerCountdown'
-import { AppDispatch, RootState } from '@/context/store'
+import { Dispatch, SetStateAction } from 'react'
 import { Button } from '@/components/atoms/Button'
-import {
-  useActivateAccountMutation,
-  useSendWelcomeEmailMutation
-} from '@/external/graphql/generated'
-import { authVar, popoverVar } from '@/external/graphql/reactiveVars'
+import { usePinCode } from './hooks/usePinCode'
 import * as classes from './stylesheet'
 
 type PinCodeProps = {
   setStepsHandler: Dispatch<SetStateAction<number>>
 }
 export const PinCode: React.FC<PinCodeProps> = ({ setStepsHandler }) => {
-  const [activateAccount, { error }] = useActivateAccountMutation()
-  const [sendWelcome] = useSendWelcomeEmailMutation()
-  const [isLoading, setIsLoading] = useState(false)
-  const [inputError, setInputError] = useState<ApolloError | undefined>()
-  const [values, setValues] = useState<string[]>(
-    Array.from({ length: 5 }).fill('', 0, 5) as string[]
-  )
-
-  const dispatch = useDispatch<AppDispatch>()
-  const { isOnCountdown, timeLeftInSeconds } = useSelector<
-  RootState,
-  MailerCountdownStates
-  >((state) => state.mailerCountdown)
-  const { startCountdown } = mailerCountdownActions
-
-  const form = document.querySelector('form') as HTMLFormElement
-  const getInput = (index: number) =>
-    document.querySelector(`input[data-index="${index}"]`) as HTMLElement
-
-  const onChangeHandler = (value: string, index: number) => {
-    if (value.length >= 5) {
-      return setValues(values.map((_, i) => value.split('')[i]))
-    }
-
-    if (value.length <= 2) {
-      const lastChar = value.charAt(value.length - 1)
-      setValues(values.map((v, i) => (i === index ? lastChar : v)))
-
-      if (index < 4 && value !== '') {
-        getInput(index + 1).focus()
-      }
-    }
-  }
-
-  const inputNavigation = (index: number, key: string) => {
-    if (index < 4) {
-      if (key === 'ArrowRight' || key === 'Delete') {
-        getInput(index + 1).focus()
-      }
-    }
-
-    if (index > 0) {
-      if (key === 'ArrowLeft') {
-        getInput(index - 1).focus()
-      }
-
-      if (key === 'Backspace' && values[index] === '') {
-        getInput(index - 1).focus()
-      }
-    }
-  }
-
-  const onKeyPressHandler = (key: string, index: number) => {
-    if (key === 'Backspace' || key === 'Delete') {
-      setValues((prev) => prev.map((v, i) => (i === index ? '' : v)))
-    }
-
-    if (key === 'Backspace' && values[index] === '') {
-      setValues((prev) => {
-        prev[index - 1] = ''
-        return prev
-      })
-    }
-
-    inputNavigation(index, key)
-  }
-
-  const onSubmitHandler = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    setIsLoading(true)
-
-    try {
-      await activateAccount({
-        variables: {
-          activationPin: values.join(''),
-          userId: getUserByToken('userId')
-        }
-      })
-
-      popoverVar.setPopover(
-        'Conta verificada com sucesso. Você está sendo redirecionado para a página inicial',
-        'success'
-      )
-
-      setIsLoading(false)
-      setStepsHandler((step) => step + 1)
-      await new Promise((resolve) => setTimeout(resolve, 1500))
-
-      authVar.login()
-    } catch (error: any) {
-      setIsLoading(false)
-      setInputError(error)
-    }
-  }
-
-  const resendEmailHandler = async () => {
-    setIsLoading(true)
-
-    try {
-      await sendWelcome({
-        variables: {
-          email: getUserByToken('email')
-        }
-      })
-
-      setIsLoading(false)
-      dispatch(startCountdown())
-    } catch (error: any) {
-      setIsLoading(false)
-      setInputError(error)
-    }
-  }
-
-  useEffect(() => {
-    if (error) {
-      setInputError(error)
-    } else {
-      setInputError(undefined)
-    }
-  }, [values, error])
-
-  useEffect(() => {
-    setInputError(undefined)
-  }, [values])
-
-  useEffect(() => {
-    if (values.join('').length === 5) {
-      form.requestSubmit()
-    }
-  }, [values])
-
-  const selectLastChar = ({
-    currentTarget
-  }: FocusEvent<HTMLInputElement> | MouseEvent<HTMLInputElement>) =>
-    currentTarget.setSelectionRange(
-      currentTarget.value.length,
-      currentTarget.value.length
-    )
+  const {
+    resendEmailHandler,
+    onKeyPressHandler,
+    timeLeftInSeconds,
+    onChangeHandler,
+    onSubmitHandler,
+    selectLastChar,
+    isOnCountdown,
+    inputError,
+    isLoading,
+    values
+  } = usePinCode({ setStepsHandler })
 
   const pinInputClass = classes.pinInput({
     state: inputError ? 'error' : 'default'
   })
+
+  const mappedInputs = values.map((value, index) => (
+    <input
+      onChange={(event) => onChangeHandler(event.target.value, index)}
+      onKeyDown={(event) => onKeyPressHandler(event.key, index)}
+      className={pinInputClass}
+      onClick={selectLastChar}
+      onFocus={selectLastChar}
+      data-testid="pin-input"
+      autoFocus={index === 0}
+      data-index={index}
+      value={value}
+      key={index}
+      type="text"
+    />
+  ))
 
   return (
     <form
@@ -176,23 +46,7 @@ export const PinCode: React.FC<PinCodeProps> = ({ setStepsHandler }) => {
       onSubmit={onSubmitHandler}
       data-testid="form"
     >
-      <div className={classes.pinCode}>
-        {values.map((value, index) => (
-          <input
-            onChange={(event) => onChangeHandler(event.target.value, index)}
-            onKeyDown={(event) => onKeyPressHandler(event.key, index)}
-            className={pinInputClass}
-            onClick={selectLastChar}
-            onFocus={selectLastChar}
-            data-testid="pin-input"
-            autoFocus={index === 0}
-            data-index={index}
-            value={value}
-            key={index}
-            type="text"
-          />
-        ))}
-      </div>
+      <div className={classes.pinCode}>{mappedInputs}</div>
 
       <Button
         loading={isLoading}
