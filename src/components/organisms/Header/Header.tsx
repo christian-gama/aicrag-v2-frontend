367f46c0e59d5f7c9e019b8c6a5bb1aa54d83fdc
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-02-03 20:07:47 -0300

    feat: renders a BackIcon if has backHandler defined

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,17 +1,22 @@
 import { useState } from 'react'
 import { formatName } from '@/helpers'
 import { getUserByToken } from '@/services/token/getUserByToken'
+import { BackIcon } from '@/components/utils/icons'
 import { LogoutIcon } from '@/components/utils/icons/LogoutIcon'
 import { QuestionIcon } from '@/components/utils/icons/QuestionIcon'
+import { SettingsIcon } from '@/components/utils/icons/SettingsIcon'
+import { H1 } from '@/components/utils/texts/H1'
+import { P } from '@/components/utils/texts/P'
 import { authVar } from '@/external/graphql/reactiveVars'
 import { About } from '../About'
 import * as classes from './stylesheet'
 
 type HeaderProps = {
   pageName: string
+  backHandler?: () => void
 }
 
-export const Header: React.FC<HeaderProps> = ({ pageName }) => {
+export const Header: React.FC<HeaderProps> = ({ pageName, backHandler }) => {
   const [isAboutOpen, setIsAboutOpen] = useState(false)
   const userName = formatName(getUserByToken('name')!)
   const onLogoutHandler = () => {
@@ -25,14 +30,36 @@ export const Header: React.FC<HeaderProps> = ({ pageName }) => {
   return (
     <>
       <div className={classes.header} data-testid="header">
-        {pageName}
-        {userName}
-        <div onClick={onLogoutHandler}>
-          <LogoutIcon />
+        <div className={classes.headerLeft}>
+          {backHandler && (
+            <div className={classes.headerBackIcon} onClick={backHandler}>
+              <BackIcon color="white" />
+            </div>
+          )}
+
+          <div className={classes.headerPageName}>
+            <H1 color="white">{pageName}</H1>
+          </div>
         </div>
 
-        <div onClick={onAboutHandler}>
-          <QuestionIcon />
+        <div className={classes.headerRight}>
+          <div className={classes.headerUserName}>
+            <P color="white">{`Olá, ${userName}!`}</P>
+          </div>
+
+          <div className={classes.headerIconGroup}>
+            <div>
+              <SettingsIcon />
+            </div>
+
+            <div onClick={onAboutHandler}>
+              <QuestionIcon />
+            </div>
+
+            <div onClick={onLogoutHandler}>
+              <LogoutIcon />
+            </div>
+          </div>
         </div>
       </div>
 
--- src/components/organisms/Header/stylesheet/Header.css.ts
+++ src/components/organisms/Header/stylesheet/Header.css.ts
@@ -2,5 +2,43 @@ import { style } from '@vanilla-extract/css'
 import { vars } from '@/components/_settings'
 
 export const header = style({
-  backgroundColor: vars.colors['navy-500']
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  backgroundColor: vars.colors['navy-500'],
+  height: '12rem'
+})
+
+export const headerLeft = style({
+  display: 'flex',
+  alignItems: 'center',
+  marginLeft: '9rem'
+})
+
+export const headerBackIcon = style({
+  ':hover': {
+    cursor: 'pointer'
+  }
+})
+
+export const headerPageName = style({
+  marginLeft: '4.4rem'
+})
+
+export const headerRight = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  marginRight: '9rem'
+})
+
+export const headerUserName = style({
+  marginRight: '4.4rem'
+})
+
+export const headerIconGroup = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  width: '16rem'
 })
