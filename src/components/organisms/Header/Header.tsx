65898c6a735861f0c17c58552dcb275a44ed4948
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-02-03 14:20:30 -0300

    feat: renders the page name

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,5 +1,13 @@
 import * as classes from './stylesheet'
 
-export const Header: React.FC = () => {
-  return <div className={classes.header} data-testid="header"></div>
+type HeaderProps = {
+  pageName: string
+}
+
+export const Header: React.FC<HeaderProps> = ({ pageName }) => {
+  return (
+    <div className={classes.header} data-testid="header">
+      {pageName}
+    </div>
+  )
 }
