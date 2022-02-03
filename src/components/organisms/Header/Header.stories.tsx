3eae90c99876ffe4318f3e0f3050fe0e286a1012
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-02-03 14:35:16 -0300

    feat: ensure story has the correct access token

--- src/components/organisms/Header/Header.stories.tsx
+++ src/components/organisms/Header/Header.stories.tsx
@@ -1,9 +1,22 @@
 import { ComponentStoryObj, ComponentMeta } from '@storybook/react'
+import { makeAccessTokenStorage } from '@/external/factories/storage/auth'
+import { mockVariables } from '@/tests/mocks'
 import { Header as HeaderComponent } from './Header'
 
 export default {
   component: HeaderComponent,
-  title: 'organisms/Header'
+  title: 'organisms/Header',
+  args: {
+    pageName: 'Any page name'
+  },
+  decorators: [
+    (story) => {
+      const accessTokenStorage = makeAccessTokenStorage()
+      accessTokenStorage.set(mockVariables.token)
+
+      return story()
+    }
+  ]
 } as ComponentMeta<typeof HeaderComponent>
 
 export const Header: ComponentStoryObj<typeof HeaderComponent> = {}
