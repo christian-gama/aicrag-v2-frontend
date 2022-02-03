0ba8cf6ef0729a827ada8ce2ffdc698f5a5d7e7d
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-02-03 19:46:24 -0300

    test: opens the About modal when clicking on QuestionIcon

--- src/components/organisms/Header/__tests__/Header.spec.tsx
+++ src/components/organisms/Header/__tests__/Header.spec.tsx
@@ -1,38 +1,42 @@
 import { makeAccessTokenStorage } from '@/external/factories/storage/auth'
 import { authVar } from '@/external/graphql/reactiveVars'
+import { OverlayRoot, renderWithProviders } from '@/tests/helpers'
 import { mockVariables } from '@/tests/mocks'
-import { render, cleanup, screen } from '@testing-library/react'
+import { cleanup, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { Header } from '..'
 
 describe('Header', () => {
   const accessTokenStorage = makeAccessTokenStorage()
+  const overlayRoot = new OverlayRoot()
 
   afterEach(() => {
     cleanup()
     accessTokenStorage.reset()
+    overlayRoot.removeOverlayRoot()
   })
 
   beforeEach(() => {
     accessTokenStorage.set(mockVariables.token)
+    overlayRoot.addOverlayRoot()
   })
 
   it('renders correctly', () => {
-    render(<Header pageName="" />)
+    renderWithProviders(<Header pageName="" />)
     const header = screen.getByTestId('header')
 
     expect(header).toBeInTheDocument()
   })
 
   it('renders the page name', () => {
-    render(<Header pageName="Any name" />)
+    renderWithProviders(<Header pageName="Any name" />)
     const pageName = screen.getByText(/any name/gi)
 
     expect(pageName).toBeInTheDocument()
   })
 
   it('renders the user name', () => {
-    render(<Header pageName="" />)
+    renderWithProviders(<Header pageName="" />)
     const userName = screen.getByText(mockVariables.name)
 
     expect(userName).toBeInTheDocument()
@@ -40,11 +44,20 @@ describe('Header', () => {
 
   it('logs the user out when click on LogoutIcon', () => {
     const logoutSpy = jest.spyOn(authVar, 'logout')
-    render(<Header pageName="" />)
+    renderWithProviders(<Header pageName="" />)
     const logoutIcon = screen.getByTestId('logout-icon')
 
     userEvent.click(logoutIcon)
 
     expect(logoutSpy).toHaveBeenCalled()
+  })
+
+  it('opens the About modal when clicking on QuestionIcon', () => {
+    renderWithProviders(<Header pageName="" />)
+    const questionIcon = screen.getByTestId('question-icon')
+
+    userEvent.click(questionIcon)
+
+    expect(screen.getByTestId('about')).toBeInTheDocument()
   })
 })
