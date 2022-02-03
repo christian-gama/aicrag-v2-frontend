f8f34df33d1a4be1329561fa25ebd1533a4a7bb3
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-02-03 14:40:06 -0300

    test: calls logout when clicking on icon

---
+++ src/components/utils/icons/LogoutIcon/__tests__/LogoutIcon.spec.tsx
@@ -0,0 +1,27 @@
+import { authVar } from '@/external/graphql/reactiveVars'
+import { render, cleanup, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { LogoutIcon } from '..'
+
+describe('LogoutIcon', () => {
+  afterEach(() => {
+    cleanup()
+  })
+
+  it('renders correctly', () => {
+    render(<LogoutIcon />)
+    const logout = screen.getByTestId('logout')
+
+    expect(logout).toBeInTheDocument()
+  })
+
+  it('calls logout when clicking on icon', () => {
+    const logoutSpy = jest.spyOn(authVar, 'logout')
+    render(<LogoutIcon />)
+    const logout = screen.getByTestId('logout')
+
+    userEvent.click(logout)
+
+    expect(logoutSpy).toHaveBeenCalled()
+  })
+})
