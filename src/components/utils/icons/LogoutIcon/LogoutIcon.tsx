d59c5e152f088741932342793334c32d5d292b0b
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-02-03 15:47:10 -0300

    feat: set the proper style to LogoutIcon

--- src/components/utils/icons/LogoutIcon/LogoutIcon.tsx
+++ src/components/utils/icons/LogoutIcon/LogoutIcon.tsx
@@ -1,23 +1,38 @@
 import { authVar } from '@/external/graphql/reactiveVars'
 import * as classes from './stylesheet'
+import { LogoutIconVariants } from './stylesheet'
 
-export const LogoutIcon: React.FC = () => {
+type LogouIconProps = {
+  color?: LogoutIconVariants['color']
+}
+
+export const LogoutIcon: React.FC<LogouIconProps> = ({ color }) => {
   const onClickHandler = () => {
     authVar.logout()
   }
 
   return (
-    <svg
-      onClick={onClickHandler}
-      xmlns="http://www.w3.org/2000/svg"
-      className={classes.logoutIcon}
+    <div
+      className={classes.logoutIconWrapper({ color })}
       data-testid="logout-icon"
-      viewBox="0 0 48 48"
+      onClick={onClickHandler}
     >
-      <path
-        d="M49,25,33,11.667v8H14.333V30.333H33v8ZM6.333,6.333H27.667V1H6.333A5.349,5.349,0,0,0,1,6.333V43.667A5.349,5.349,0,0,0,6.333,49H27.667V43.667H6.333Z"
-        transform="translate(-1 -1)"
-      />
-    </svg>
+      <svg
+        className={classes.logoutIconRecipe({
+          color
+        })}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 48 48"
+      >
+        <path
+          d="M49,25,33,11.667v8H14.333V30.333H33v8ZM6.333,6.333H27.667V1H6.333A5.349,5.349,0,0,0,1,6.333V43.667A5.349,5.349,0,0,0,6.333,49H27.667V43.667H6.333Z"
+          transform="translate(-1 -1)"
+        />
+      </svg>
+    </div>
   )
+}
+
+LogoutIcon.defaultProps = {
+  color: 'primary'
 }
--- src/components/utils/icons/LogoutIcon/stylesheet/LogoutIcon.css.ts
+++ src/components/utils/icons/LogoutIcon/stylesheet/LogoutIcon.css.ts
@@ -1,3 +1,63 @@
-import { style } from '@vanilla-extract/css'
+import { recipe, RecipeVariants } from '@vanilla-extract/recipes'
+import { vars } from '@/components/_settings'
 
-export const logoutIcon = style({})
+export const logoutIconWrapper = recipe({
+  base: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    paddingLeft: '4px',
+    width: '3.6rem',
+    height: '3.6rem'
+  },
+
+  variants: {
+    color: {
+      primary: {
+        backgroundColor: 'white'
+      },
+
+      secondary: {
+        backgroundColor: vars.colors['navy-500']
+      }
+    }
+  }
+})
+
+export const logoutIconRecipe = recipe({
+  base: {
+    width: '2rem'
+  },
+
+  variants: {
+    color: {
+      primary: {
+        color: vars.colors['navy-500'],
+        fill: vars.colors['navy-500'],
+
+        ':hover': {
+          filter: 'brightness(130%)'
+        }
+      },
+
+      secondary: {
+        color: vars.colors.white,
+        fill: vars.colors.white,
+
+        ':hover': {
+          filter: 'brightness(90%)'
+        }
+      }
+    }
+  },
+
+  defaultVariants: {
+    color: 'primary'
+  }
+})
+
+export type LogoutIconVariants = NonNullable<
+RecipeVariants<typeof logoutIconRecipe>
+>
