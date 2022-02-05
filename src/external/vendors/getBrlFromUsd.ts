486e196f903e23214c99bcb5c67989bb6da921d7
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-02-05 16:34:59 -0300

    refactor: does not catch errors inside function

--- src/external/vendors/getBrlFromUsd.ts
+++ src/external/vendors/getBrlFromUsd.ts
@@ -1,13 +1,9 @@
 export const getBrlFromUsd = async () => {
-  try {
-    const res = await fetch(
-      'https://economia.awesomeapi.com.br/json/last/USD-BRL'
-    )
+  const res = await fetch(
+    'https://economia.awesomeapi.com.br/json/last/USD-BRL'
+  )
 
-    const BRL = +(await res.json()).USDBRL.ask
+  const BRL = +(await res.json()).USDBRL.ask
 
-    return BRL
-  } catch (err) {
-    return 5
-  }
+  return BRL
 }
