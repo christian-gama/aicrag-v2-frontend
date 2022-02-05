621f3833603ad6a0d48fd0f15cdf00a7e3843f90
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-02-05 01:54:58 -0300

    test: renders NewTask correctly

---
+++ src/components/templates/NewTask/__tests__/NewTask.spec.tsx
@@ -0,0 +1,82 @@
+import { cleanup, fireEvent, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { makeAccessTokenStorage } from '@/external/factories/storage/auth'
+import { popoverVar } from '@/external/graphql/reactiveVars'
+import {
+  MockDate,
+  OverlayRoot,
+  renderWithProviders,
+  waitFetch
+} from '@/tests/helpers'
+import { mockVariables } from '@/tests/mocks'
+import { createTaskMock } from '@/tests/mocks/queries'
+
+describe('NewTask', () => {
+  let NewTask: any
+  const accessTokenStorage = makeAccessTokenStorage()
+  const mockDate = new MockDate(2022, 1, 1, 0, 0)
+  const overlayRoot = new OverlayRoot()
+
+  const getElement = (selector: string) =>
+    screen.getByTestId('new-task').querySelector(selector) as HTMLElement
+
+  const commentary = () => getElement('textarea[name="commentary"]')
+  const duration = () => getElement('input[name="duration"]')
+  const status = () => getElement('select[name="status"]')
+  const taskId = () => getElement('input[name="taskId"]')
+  const type = () => getElement('select[name="type"]')
+
+  afterEach(() => {
+    overlayRoot.removeOverlayRoot()
+    accessTokenStorage.reset()
+    mockDate.reset()
+    cleanup()
+  })
+
+  beforeEach(async () => {
+    accessTokenStorage.set(mockVariables.token)
+    overlayRoot.addOverlayRoot()
+    mockDate.mock()
+
+    // Imports dinamically so it can have the date mocked, as it's using DefaultProps
+    NewTask = (await import('../NewTask')).NewTask
+  })
+
+  it('renders correctly', () => {
+    renderWithProviders(<NewTask />)
+    const newTask = screen.getByTestId('new-task')
+
+    expect(newTask).toBeInTheDocument()
+  })
+
+  it('changes the duration to 2.4 minutes when change type to QA', () => {
+    renderWithProviders(<NewTask />)
+
+    userEvent.selectOptions(type(), 'QA')
+
+    expect(duration()).toHaveValue(2.4)
+  })
+
+  it('changes the duration to 30 minutes when change type to TX', () => {
+    renderWithProviders(<NewTask />)
+
+    userEvent.selectOptions(type(), 'TX')
+
+    expect(duration()).toHaveValue(30)
+  })
+
+  it('submits the form', async () => {
+    const popoverSpy = jest.spyOn(popoverVar, 'setPopover')
+    renderWithProviders(<NewTask />, { apolloMocks: [createTaskMock()] })
+    const form = screen.getByTestId('form')
+
+    userEvent.type(taskId(), mockVariables.taskId)
+    userEvent.type(commentary(), mockVariables.commentary)
+    userEvent.selectOptions(type(), mockVariables.type)
+    userEvent.selectOptions(status(), mockVariables.status)
+    fireEvent.submit(form)
+    await waitFetch()
+
+    expect(popoverSpy).toHaveBeenCalledWith(expect.anything(), 'success')
+  })
+})
