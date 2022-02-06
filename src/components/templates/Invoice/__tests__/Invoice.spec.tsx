5a5c2a65fbdad7aabbf8d506fad9ec154da834d0
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-02-06 15:20:47 -0300

    test: ensure Invoice is working correctly

---
+++ src/components/templates/Invoice/__tests__/Invoice.spec.tsx
@@ -0,0 +1,51 @@
+import { makeAccessTokenStorage } from '@/external/factories/storage/auth'
+import { renderWithProviders, waitFetch } from '@/tests/helpers'
+import { mockVariables } from '@/tests/mocks'
+import { getAllInvoicesMock } from '@/tests/mocks/queries'
+import { cleanup, screen } from '@testing-library/react'
+import jwtDecode from 'jwt-decode'
+import { Invoice } from '..'
+
+jest.mock('jwt-decode')
+const jwtDecodeMock = jwtDecode as jest.Mock
+
+describe('Invoice', () => {
+  afterEach(() => {
+    cleanup()
+  })
+
+  beforeEach(() => {
+    jwtDecodeMock.mockReturnValue({ currency: 'BRL' })
+  })
+
+  it('renders correctly', async () => {
+    renderWithProviders(<Invoice />, { apolloMocks: [getAllInvoicesMock()] })
+    await waitFetch()
+    const invoice = screen.getByTestId('invoice')
+
+    expect(invoice).toBeInTheDocument()
+  })
+
+  it('returns null if there is no data', async () => {
+    renderWithProviders(<Invoice />, {
+      apolloMocks: [getAllInvoicesMock(new Error())]
+    })
+    await waitFetch()
+    const invoice = screen.queryByTestId('invoice')
+
+    expect(invoice).not.toBeInTheDocument()
+  })
+
+  it('renders a table with the correct currency symbol', async () => {
+    makeAccessTokenStorage().set(mockVariables.token)
+    jwtDecodeMock.mockImplementation(() => ({ currency: 'BRL' }))
+
+    renderWithProviders(<Invoice />, {
+      apolloMocks: [getAllInvoicesMock()]
+    })
+    await waitFetch()
+    const currency = screen.getByText(/^R\$ 50$/i)
+
+    expect(currency).toBeInTheDocument()
+  })
+})
