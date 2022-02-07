cece7c045ad8cd4714ce0057a90bfd7b0888b35b
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-02-07 05:20:55 -0300

    feat: add updateEmailByPin mock

---
+++ tests/mocks/queries/updateEmailByPin.mock.ts
@@ -0,0 +1,23 @@
+import { MockedResponse } from '@apollo/client/testing'
+import { UpdateEmailByPinDocument } from '@/external/graphql/generated'
+import { mockVariables } from '..'
+import { userFragmentMock } from '../fragments'
+
+export const updateEmailByPin = (
+  error?: Error
+): MockedResponse<Record<string, any>> => ({
+  request: {
+    query: UpdateEmailByPinDocument,
+    variables: {
+      emailPin: mockVariables.activationPin
+    }
+  },
+  result: {
+    data: {
+      updateEmailByPin: {
+        ...userFragmentMock
+      }
+    }
+  },
+  error
+})
