07fe9d1e8a370ab7581ed68cc75116f0f0fbe107
Christian Gama - LInux <christiangama.dev@gmail.com>
2022-02-08 13:56:01 -0300

    test: ensure deletes the task by clicking on trash icon

--- src/components/templates/InvoiceDetails/__tests__/InvoiceDetails.spec.tsx
+++ src/components/templates/InvoiceDetails/__tests__/InvoiceDetails.spec.tsx
@@ -1,9 +1,11 @@
 import { makeAccessTokenStorage } from '@/external/factories/storage/auth'
+import { popoverVar } from '@/external/graphql/reactiveVars'
 import { OverlayRoot, renderWithProviders, waitFetch } from '@/tests/helpers'
 import { mockVariables } from '@/tests/mocks'
 import { taskFragmentMock } from '@/tests/mocks/fragments'
-import { getInvoiceByMonthMock } from '@/tests/mocks/queries'
+import { deleteTaskMock, getInvoiceByMonthMock } from '@/tests/mocks/queries'
 import { cleanup, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import jwtDecode from 'jwt-decode'
 import { InvoiceDetails } from '..'
 
@@ -101,5 +103,40 @@ describe('InvoiceDetails', () => {
     const invoiceDetails = screen.getByText(/^--$/)
 
     expect(invoiceDetails).toBeInTheDocument()
+  })
+
+  it('deletes the task', async () => {
+    const popoverSpy = jest.spyOn(popoverVar, 'setPopover')
+    renderWithProviders(<InvoiceDetails />, {
+      apolloMocks: [
+        getInvoiceByMonthMock(),
+        deleteTaskMock({ id: mockVariables.uuid })
+      ]
+    })
+    await waitFetch()
+    const deleteButton = screen.getByTestId('trash-icon')
+    const deleteAction = () => screen.getByTestId('alert-action-button')
+
+    userEvent.click(deleteButton)
+    userEvent.click(deleteAction())
+    await waitFetch()
+
+    expect(popoverSpy).toHaveBeenCalled()
+  })
+
+  it('closes the Alert when clicking on cancel', async () => {
+    renderWithProviders(<InvoiceDetails />, {
+      apolloMocks: [getInvoiceByMonthMock(), deleteTaskMock()]
+    })
+    await waitFetch()
+    const deleteButton = screen.getByTestId('trash-icon')
+    const cancelAction = () => screen.getByTestId('alert-cancel-button')
+    const alert = () => screen.queryByTestId('alert')
+
+    userEvent.click(deleteButton)
+    userEvent.click(cancelAction())
+    await waitFetch()
+
+    expect(alert()).not.toBeInTheDocument()
   })
 })
